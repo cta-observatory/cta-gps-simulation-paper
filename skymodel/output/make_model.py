3a2652130c2939299a3f8dc4e933290b6ef2f398
Luigi Tibaldo <luigi.tibaldo@irap.omp.eu>
2019-08-26 18:02:50 +0200

    prepared structure to add source information

--- skymodel/output/make_model.py
+++ skymodel/output/make_model.py
@@ -1,5 +1,5 @@
-import numpy as np
 import gammalib
+import numpy as np
 from gammapy.catalog import SourceCatalogGammaCat
 
 # inputs from external sources
@@ -15,20 +15,28 @@ for source in gammacat:
     # retain only sources with known spectral model
     # and centered within 10 degrees from the galactic pla
     if not source['spec_type'] == 'none' and (np.abs(source['glat']) < 10.):
+        skip = False
         # retrieve source spatial model
         if source['morph_type'] == 'point' or source['morph_type'] == 'none':
             # source with morphology none are treated as point sources
             ra = np.double(source['ra'])
             dec = np.double(source['dec'])
-            spatial = gammalib.GModelSpatialPointSource(ra,dec)
+            spatial = gammalib.GModelSpatialPointSource(ra, dec)
         else:
-            print('WARNING: source {} from gamma-cat has spatial model of type {} which is not implemented'.format(source['common_name'],source['morph_type']))
+            print(
+                'WARNING: source {} from gamma-cat has spatial model of type {} which is not implemented'.format(
+                    source['common_name'], source['morph_type']))
+            skip = True
+        # retrieve source spectral model
 
+        # put together and append to model container
+        if not skip:
+            gammacat_ids.append(source['source_id'])
 
 
 # add CTA background
 # power law spectral correction with pivot energy at 1 TeV
-spectral = gammalib.GModelSpectralPlaw(1,0,gammalib.GEnergy(1,'TeV'))
+spectral = gammalib.GModelSpectralPlaw(1, 0, gammalib.GEnergy(1, 'TeV'))
 # Irf Background
 bkgmodel = gammalib.GCTAModelIrfBackground(spectral)
 bkgmodel.name('Background')
