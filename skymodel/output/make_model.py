2e37800b361b6e8a42a153b81d0b03b400fc6f71
Luigi Tibaldo <luigi.tibaldo@irap.omp.eu>
2019-08-27 10:44:57 +0200

    working on spatial models

--- skymodel/output/make_model.py
+++ skymodel/output/make_model.py
@@ -17,11 +17,37 @@ for source in gammacat:
     if not source['spec_type'] == 'none' and (np.abs(source['glat']) < 10.):
         skip = False
         # retrieve source spatial model
+        # source direction
+        ra = np.double(source['ra'])
+        dec = np.double(source['dec'])
+        src_dir = gammalib.GSkyDir()
+        src_dir.radec_deg(ra, dec)
         if source['morph_type'] == 'point' or source['morph_type'] == 'none':
             # source with morphology none are treated as point sources
-            ra = np.double(source['ra'])
-            dec = np.double(source['dec'])
-            spatial = gammalib.GModelSpatialPointSource(ra, dec)
+            spatial = gammalib.GModelSpatialPointSource(src_dir)
+        elif source['morph_type'] == 'gauss':
+            # Gaussian morphology
+            sigma = np.double(source['morph_sigma'])
+            if np.isnan(source['morph_sigma2']):
+                # symmetric Gaussian
+                spatial = gammalib.GModelSpatialRadialGauss(src_dir, sigma)
+            else:
+                # elliptical Gaussian
+                # WARNING: not sure position angle definition in gamma-cat matches gammalib, emailed Christoph on 27/08/2019 to ask (LT)
+                if source['morph_frame'] == 'radec':
+                    # morphology defined in celestial coordinates
+                    sigma2 = np.double(source['morph_sigma2'])
+                    pa = np.double(source['morph_pa'])
+                    spatial = gammalib.GModelSpatialEllipticalGauss(src_dir, sigma, sigma2, pa)
+                if source['morph_frame'] == 'galactic':
+                    # need to implement!!!
+                    pass
+                else:
+                    print('WARNING: source {} from gamma-cat has spatial model frame of type {} which is not implemented'.format(
+                            source['common_name'], source['morph_frame']))
+                    skip = True
+        elif source['morph_type'] == 'shell':
+
         else:
             print(
                 'WARNING: source {} from gamma-cat has spatial model of type {} which is not implemented'.format(
@@ -32,7 +58,6 @@ for source in gammacat:
         # put together and append to model container
         if not skip:
             gammacat_ids.append(source['source_id'])
-
 
 # add CTA background
 # power law spectral correction with pivot energy at 1 TeV
