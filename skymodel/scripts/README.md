7964c61d0d1fd6383c52febeadea52a06d6a12d9
Luigi Tibaldo <luigi.tibaldo@irap.omp.eu>
2019-08-28 16:18:43 +0200

    make script

--- skymodel/scripts/README.md
+++ skymodel/scripts/README.md
@@ -1,1 +1,7 @@
-Scripts not specific to any source type, in particular those used to assemble the final model
+# Scripts
+
+Scripts not specific to any source type, in particular those used to assemble the final model.
+
+To generate the final model type
+
+python make.py
---
+++ skymodel/scripts/make.py
@@ -0,0 +1,17 @@
+import os
+
+# clean outout directory
+outdir = '../output'
+os.system('rm {}/*.xml'.format(outdir))
+os.system('rm {}/*.fits'.format(outdir))
+os.system('rm {}/*.txt'.format(outdir))
+
+# run all preliminary scripts to generate model components
+
+# templates
+os.chdir('../known-sources/templates')
+os.system('python make_ic443_model.py')
+
+# assemble final model
+os.chdir('../../scripts')
+os.system('python make_model.py')
